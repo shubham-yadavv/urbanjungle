25b3636079b6461ff6f755ef4c18db72510554a6
shubham <shubham22121@gmail.com>
2022-05-26 01:32:20 +0530

    make it better

--- customer/views.py
+++ customer/views.py
@@ -162,3 +162,18 @@ class Route(View):
             "google_api_key": settings.GOOGLE_API_KEY,
             "base_counrty": settings.BASE_COUNTRY}
         return render(request, 'customer/route.html', context)
+
+
+# class Map(View):
+#     def get(self, reque
+#         if lat_a and lat_b and lat_c and lat_d:
+# 		directions = Directions(st):
+#         lat_a = request.GET.get("lat_a", None)
+# 	    long_a = request.GET.get("long_a", None)
+#         lat_b = request.GET.get("lat_b", None)
+# 	    long_b = request.GET.get("long_b", None)
+
+# 			lat_a= lat_a,
+# 			long_a=long_a,
+# 			lat_b = lat_b,
+# 			long_b=long_b,
