e348fbc74f6db66ca8ad1e35880c178e82152eba
shubham <shubham22121@gmail.com>
2022-05-26 01:41:18 +0530

    make it better

--- deliver/settings.py
+++ deliver/settings.py
@@ -6,7 +6,7 @@ SECRET_KEY = '(b%y6my(@zizf(agz!*6j+picf^!%tfikl#8hk3@+=e!s4a8bo'
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','urbanjungle-app.herokuapp.com']
 
 
 AUTHENTICATION_BACKENDS = [
